73ed4dc932c9b766a7f0a207e1afde5098ce4cf0
ramseyelletht <ramseyellethy2@gmail.com>
2024-02-07 16:05:06 -0600

    Starting R

---
+++ Intro.R
@@ -0,0 +1,15 @@
+# Welcome to R! 
+
+# R is an open sourced, command based data manipulation tool. 
+# There are some really powerful calculation and visual technologies 
+# available just one google search away 
+#
+# This intro will take a look at 
+
+# Data Modeling 
+# Data Analysis and Regression 
+# Data Visualization 
+
+# *Data Modeling* 
+
+# Import a data set! 
